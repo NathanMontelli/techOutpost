f1cfbf34b66b3e9f468b5c71202ee1c0ac90ff27
Nathan Montelli <nathanmontelli@gmail.com>
2022-01-02 20:46:47 -0800

    server and folders/files created

---
+++ models/comment.js
@@ -0,0 +1,9 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Comment extends Model { }
+Comment.init({
+
+})
+
+module.exports = Comment
---
+++ models/post.js
@@ -0,0 +1,9 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Post extends Model { }
+Post.init({
+
+})
+
+module.exports = Post
